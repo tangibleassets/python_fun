75ec61b3e81292905784651c5e4dd797af9624a1
Bart Atkins <bart.atkins@meddius.com>
2017-04-12 18:17:25 -0400

    Making the game accept user input.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,7 +10,9 @@
 
 
 # Global var to store board values
-my_board = {1:' ',2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '}
+my_board = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '}
+game_over = False
+
 
 def print_board(v):
     print '-----------------------'
@@ -20,22 +22,24 @@ def print_board(v):
     print '-----------      ------'
     print ' ' + v[7] + ' | ' + v[8] + ' | ' + v[9] + '       7|8|9'
     print '-----------------------'
+    print ''
 
-print_board(my_board)
 
 def make_a_move(location, value):
     my_board[location] = value
     print_board(my_board)
     check_game_status(my_board)
 
+
 def clear_the_board():
     for keys in my_board:
         my_board[keys] = ' '
-    print_board(my_board)
-    
+
+
 # Check if the game is won,tied, lost, or ongoing.
 def check_game_status(b):
-    winning_position_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]    
+    winning_position_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    global game_over
     for pos in winning_position_combos:
         x = b[pos[0]].strip().lower()
         y = b[pos[1]].strip().lower()
@@ -43,13 +47,34 @@ def check_game_status(b):
         if x != '' and y != '' and z != '':
             if x == y == z:
                 print 'Player "' + x + '" you win !!!!'
-                clear_the_board()
-                print 'Ready to play again?'
+                print 'If you would like to play again - enter \'y\' at the next prompt'
+                game_over = True
                 break
-    count = 0
-    for value in b:
-        if b[value].strip() == '':
-            count += 1
-    if count == 0:
-        print "Tie Game :(  Try again?"
-        clear_the_board()
+    if not game_over:
+        count = 0
+        for value in b:
+            if b[value].strip() == '':
+                count += 1
+        if count == 0:
+            print "Tie Game :(  Try again?"
+            game_over = True
+
+
+def yes_lets_play():
+    global game_over
+    game_over = False
+    return raw_input("Would you like to play a game? y/n:").lower().startswith('y')
+
+
+while yes_lets_play():
+    clear_the_board()
+    print_board(my_board)
+    while yes_lets_play:
+        x_or_o = raw_input("Enter either x or o:").lower()
+        position = raw_input("Enter position numnber [1-9]")
+        pos = int(position)
+        make_a_move(pos, x_or_o)
+        if game_over:
+            break
+else:
+    print 'Thanks for playing - have a nice day!'
